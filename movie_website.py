11d79ae4ae0c56785baf70076d9322d66868e276
Suraj Nair <suraj.jayraman@gmail.com>
2017-06-25 14:11:36 +0530

    ML Basic ND Initial Commit

---
+++ movie_website.py
@@ -0,0 +1,9 @@
+
+import movies
+import movielist
+import fresh_tomatoes
+
+#represents the list of favoutite movies
+movie_list = [movielist.toy_story, movielist.avatar, movielist.ratatouille, movielist.midnight_in_paris, movielist.fast_furious, movielist.school_of_rock]
+
+fresh_tomatoes.open_movies_page(movie_list)
